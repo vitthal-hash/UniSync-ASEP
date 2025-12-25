5ba6132f8c97dd23bc12ed57443c9bfb38a02546
Vitthal More <vm7368514@gmail.com>
2025-12-25 14:10:45 +0530

    fix

--- backend/controllers/analyticsController.js
+++ backend/controllers/analyticsController.js
@@ -347,14 +347,15 @@ for (const m of members) {
     `,
     [m.user_id, groupId, IE, RE, SV, EC, CWES]
   );
-}
-await pool.execute(
+  await pool.execute(
   `
   INSERT INTO user_cwes_history (user_id, group_id, cwes_score)
   VALUES (?, ?, ?)
   `,
   [m.user_id, groupId, CWES]
 );
+}
+
 
 
 
